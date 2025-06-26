3f1c9499e40db9247fda2a009c860df526b44ea9
brndmgd <brndmgdwastaken@gmail.com>
2025-06-26 17:09:09 +0600

    feat: string extension method for palindromes

---
+++ task01/StringExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace task01;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input)) return false;
+
+        input = input.ToLower();
+        string formattedInput = "";
+
+        foreach (char c in input)
+        {
+            if (!char.IsPunctuation(c) && !char.IsWhiteSpace(c))
+            {
+                formattedInput += c;
+            }
+        }
+
+        char[] charArray = formattedInput.ToCharArray();
+        Array.Reverse(charArray);
+        string reversedInput = string.Join("", charArray);
+
+        return formattedInput == reversedInput;
+    }
+}
