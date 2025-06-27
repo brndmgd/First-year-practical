4be12cd7c1029ef358fc7fbd328ce04767de043b
brndmgd <brndmgdwastaken@gmail.com>
2025-06-27 23:44:52 +0600

    feat: Fire, MoveForward, Rotate methods for ISpaceship implementation

--- task04/SpaceWar.cs
+++ task04/SpaceWar.cs
@@ -13,55 +13,68 @@ public class Cruiser : ISpaceship
 {
     public int Speed { get; }
     public int FirePower { get; }
-
-    
+    public (double X, double Y) Position;
+    public int Angle;
+    public bool Fired;
 
     public Cruiser()
     {
         Speed = 50;
         FirePower = 100;
+        Position = (0, 0);
+        Angle = 0;
+        Fired = false;
     }
 
     public void Fire()
     {
-        throw new NotImplementedException();
+        Fired = true;
     }
 
     public void MoveForward()
     {
-        throw new NotImplementedException();
+        double radians = Angle * (Math.PI / 180);
+        Position.X += Math.Cos(radians) * Speed;
+        Position.Y += Math.Sin(radians) * Speed;
     }
 
     public void Rotate(int angle)
     {
-        throw new NotImplementedException();
+        Angle = (Angle + angle) % 360;
     }
 }
 
 public class Fighter : ISpaceship
 {
     public int Speed { get; }
-
     public int FirePower { get; }
+    public (double X, double Y) Position;
+    public int Angle;
+    public bool Fired;
 
     public Fighter()
     {
         Speed = 100;
         FirePower = 50;
+        Position = (0, 0);
+        Angle = 0;
+        Fired = false;
     }
 
     public void Fire()
     {
-        throw new NotImplementedException();
+        Fired = true;
     }
 
     public void MoveForward()
     {
-        throw new NotImplementedException();
+        double radians = Angle * (Math.PI / 180);
+        Position.X += Math.Cos(radians) * Speed;
+        Position.Y += Math.Sin(radians) * Speed;
     }
 
     public void Rotate(int angle)
     {
-        throw new NotImplementedException();
+        Angle = Math.Abs(Angle + angle) % 360;
     }
 }
