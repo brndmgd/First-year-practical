fe78097563200f56c134e1ad8d578e63bcd301f7
brndmgd <brndmgdwastaken@gmail.com>
2025-07-05 20:53:52 +0600

    test: added test for ReflectionHelper

--- task07tests/AttributesTest.cs
+++ task07tests/AttributesTest.cs
@@ -8,7 +8,9 @@ public class AttributeReflectionTests
     public void Class_HasDisplayNameAttribute()
     {
         var type = typeof(SampleClass);
+
         var attribute = type.GetCustomAttribute<DisplayNameAttribute>();
+
         Assert.NotNull(attribute);
         Assert.Equal("Пример класса", attribute.DisplayName);
     }
@@ -17,7 +19,9 @@ public class AttributeReflectionTests
     public void Method_HasDisplayNameAttribute()
     {
         var method = typeof(SampleClass).GetMethod("TestMethod");
+
         var attribute = method?.GetCustomAttribute<DisplayNameAttribute>();
+
         Assert.NotNull(attribute);
         Assert.Equal("Тестовый метод", attribute.DisplayName);
     }
@@ -26,7 +30,9 @@ public class AttributeReflectionTests
     public void Property_HasDisplayNameAttribute()
     {
         var prop = typeof(SampleClass).GetProperty("Number");
+
         var attribute = prop?.GetCustomAttribute<DisplayNameAttribute>();
+
         Assert.NotNull(attribute);
         Assert.Equal("Числовое свойство", attribute.DisplayName);
     }
@@ -35,9 +41,26 @@ public class AttributeReflectionTests
     public void Class_HasVersionAttribute()
     {
         var type = typeof(SampleClass);
+
         var attribute = type.GetCustomAttribute<VersionAttribute>();
+
         Assert.NotNull(attribute);
         Assert.Equal(1, attribute.Major);
         Assert.Equal(0, attribute.Minor);
+    }
+
+    [Fact]
+    public void ReflectionHelper_OutputsCorrectInfo()
+    {
+        var output = new StringWriter();
+        Console.SetOut(output);
+        var type = typeof(SampleClass);
+
+        type.PrintTypeInfo();
+
+        Assert.Contains("Класс: Пример класса", output.ToString());
+        Assert.Contains("Версия: 1.0", output.ToString());
+        Assert.Contains("Метод TestMethod: Тестовый метод", output.ToString());
+        Assert.Contains("Свойство Number: Числовое свойство", output.ToString());
     }
 }
