466616383fc4efd137391342ef2fdad9551432cb
brndmgd <brndmgdwastaken@gmail.com>
2025-07-15 11:00:16 +0600

    style: remove comments

--- task14/DefiniteIntergral.cs
+++ task14/DefiniteIntergral.cs
@@ -3,21 +3,12 @@
 using System;
 using System.Threading;
 
-//
-// Вычисление определенного интеграла
-//
 public static class DefiniteIntegral
 {
     private static int usingResource = 0;
     private static double Area = 0;
 
     static Barrier? barrier;
-    //
-    // a, b - границы отрезка, на котором происходит вычисление опредленного интеграла
-    // function - функция, для которой вычисляется определнный интеграл
-    // step - размер одного шага разбиения
-    // threadsNumber - число потоков, которые используются для вычислений
-    //
     public static double Solve(double a, double b, Func<double, double> function, double step, int threadsnumber)
     {
         barrier = new Barrier(threadsnumber + 1);
