c1f2e12c5476d2a6d624ae60bf859d8e6e111669
brndmgd <brndmgdwastaken@gmail.com>
2025-07-19 01:45:26 +0600

    feat: ServerThread

---
+++ task17/ICommand.cs
@@ -0,0 +1,6 @@
+﻿namespace task17;
+
+public interface ICommand
+{
+    public void Execute();
+}
---
+++ task17/ServerThread.cs
@@ -0,0 +1,62 @@
+namespace task17;
+
+using System.Collections.Concurrent;
+using System.Reflection.Metadata;
+using System.Threading;
+
+public class ServerThread
+{
+    private Thread _thread;
+    private bool _softStopRequested = false;
+    private bool _hardStopRequested = false;
+    private BlockingCollection<ICommand> _commands = new BlockingCollection<ICommand>();
+
+    public ServerThread()
+    {
+        _thread = new Thread(() => ExecuteCommands());
+    }
+
+    public void Start()
+    {
+        if (_thread.ThreadState == ThreadState.Running) return;
+        _thread.Start();
+    }
+
+    public void AddCommand(ICommand command)
+    {
+        _commands.Add(command);
+    }
+
+    public void ExecuteCommands()
+    {
+        while (!_hardStopRequested)
+        {
+            if (_softStopRequested && _commands.Count == 0)
+                break;
+
+            ICommand curCommand = _commands.Take();
+
+            try
+            {
+                curCommand.Execute();
+            }
+            catch (Exception ex)
+            {
+                ExceptionHandler.Handle(ex, curCommand);
+            }
+        }
+    }
+
+    public void RequestHardStop() => _hardStopRequested = true;
+    public void RequestSoftStop() => _softStopRequested = true;
+    public bool isCurrentThread() => _thread == Thread.CurrentThread;
+}
+
+
+public static class ExceptionHandler
+{
+    public static void Handle(Exception exception, ICommand command)
+    {
+        Console.WriteLine($"Команда {command.GetType().Name} не была выполнена: {exception.Message}");
+    }
+}
