dbd38237c2dbb51a68169ef4e2fcfe6e88de6a39
brndmgd <brndmgdwastaken@gmail.com>
2025-07-19 04:18:32 +0600

    feat: long operation demonstration

---
+++ task19/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace task19;
+
+using ServerLib;
+using task17;
+
+public class TestCommand(int id, int expectedCount) : ICommand
+{
+    public int Counter = 0;
+
+    public bool IsCompleted => Counter == expectedCount;
+
+    public void Execute()
+    {
+        Console.WriteLine($"Поток {id} вызов {++Counter}");
+    }
+}
+
+public class LongOperation
+{
+    public static void Main()
+    {
+        ServerThread serverThread = new ServerThread();
+
+        for (int i = 0; i < 5; i++)
+        {
+            serverThread.AddCommand(new TestCommand(i, 3));
+        }
+
+        serverThread.Start();
+        Thread.Sleep(500);
+    }
+}
---
+++ task19tests/LongOperationTest.cs
@@ -0,0 +1,23 @@
+﻿namespace task19tests;
+
+using task19;
+
+public class LongOperationTest
+{
+    [Fact]
+    public void LongOperation_PrintsCorrectInformation()
+    {
+        StringWriter output = new StringWriter();
+        Console.SetOut(output);
+
+        LongOperation.Main();
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 1; j < 4; j++)
+            {
+                Assert.Contains($"Поток {i} вызов {j}", output.ToString());
+            }
+        }
+    }
+}
